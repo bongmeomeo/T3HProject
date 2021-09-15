cd921f4e716f9fbdc268a36d8f02132def204007
bongmeomeo <lhongquan.1998@gmail.com>
2021-09-15 21:28:24 +0700

    first commit

---
+++ src/main/java/com/T3H/T3HApplication.java
@@ -0,0 +1,13 @@
+package com.T3H;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T3HApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T3HApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/T3H/T3HApplicationTests.java
@@ -0,0 +1,13 @@
+package com.T3H;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T3HApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
